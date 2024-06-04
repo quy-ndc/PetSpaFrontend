56d51af4de0439c66b762c393aefe9cbaaf52d09
reiichan23 <thaottpse173309@fpt.edu.vn>
2024-06-04 13:00:01 +0700

    add about us page

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,18 @@ import EmptyLayout from "./layouts/EmptyLayout/EmptyLayout";
 import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import BookingPage from "./pages/BookingPage/booking-page";
+import AboutUsPage from "./pages/AboutUsPage/about-us";
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<EmptyLayout />}>
-                <Route path="login" element={<Login />} />
-                <Route path="register" element={<Register />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
                 <Route path="/booking" element={<BookingPage />} />
+                <Route path="/about-us" element={<AboutUsPage />} />
+
+
             </Route>
         </Routes>
     );
