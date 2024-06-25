1ca960db5712bea0d5a25f9296574dcabe5e691c
QueThanhHoa <nguyenhuybach01@gmail.com>
2024-06-25 14:01:54 +0700

    delete header and footer

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ import HoslisticPage from "./pages/ServicePage/HoslisticPage/hoslistic-page";
 import GroomingPage from "./pages/ServicePage/GroomingPage/grooming-page";
 import PricingPage from "./pages/ServicePage/PricingPage/pricing-page";
 import ComboPage from "./pages/ServicePage/ComboPage/combo-page";
-// import Home from "./pages/HomePage/home";
+import Home from "./pages/HomePage/home";
 import AdminDashboard from "./pages/AdminPage/admin-dashboard";
 import DoctorLayout from "./components/layout/doctor-layout";
 import DoctorDashboard from "./pages/DoctorPage/doctor-dashboard";
@@ -29,7 +29,7 @@ function App() {
             {/* TEST */}
             <Route path='/scheduler' element={<SchedulerView />} />
             <Route path="/" element={<EmptyLayout />}>
-            {/* <Route path="/home" element={<Home />} /> */}
+                <Route path="/home" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
                 <Route path="/booking" element={<BookingPage />} />
