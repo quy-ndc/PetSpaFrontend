8283ee7ea3b0983a2055534d6f6ac293e40303ad
Quý <toilachungquy6789@gmail.com>
2024-07-13 03:02:38 +0700

    fix deploy

--- src/pages/ProfilePage/profile.tsx
+++ src/pages/ProfilePage/profile.tsx
@@ -2,14 +2,19 @@ import React from 'react'
 import { UserProfileProvider } from './UserProfie/user-profile-context'
 import UserSideBar from './UserProfie/UserProfileSide/user-sidebar'
 import UserProfileMainContent from './UserProfie/UserProfileMain/user-profile-main'
-import './profile.css'
 
 const Profile: React.FC = () => {
 
     return (
         <div className="user-profile-container">
             <div className='user-profile-main-content'>
-                <div className='user-profile-nav-content-container'>
+                <div
+                    className='user-profile-nav-content-container'
+                    style={{
+                        display: 'flex',
+                        alignItems: 'center',
+                        paddingTop: 10
+                    }}>
                     <UserProfileProvider>
                         <UserSideBar />
                         <UserProfileMainContent />
