7eef7fae1fabb299a035a1b5a72580c2d26172f1
Quý <toilachungquy6789@gmail.com>
2024-07-13 03:08:27 +0700

    fix profile page styling

--- src/pages/ProfilePage/profile.tsx
+++ src/pages/ProfilePage/profile.tsx
@@ -2,19 +2,14 @@ import React from 'react'
 import { UserProfileProvider } from './UserProfie/user-profile-context'
 import UserSideBar from './UserProfie/UserProfileSide/user-sidebar'
 import UserProfileMainContent from './UserProfie/UserProfileMain/user-profile-main'
+import './user-profile.css'
 
 const Profile: React.FC = () => {
 
     return (
         <div className="user-profile-container">
-            <div className='user-profile-main-content'>
-                <div
-                    className='user-profile-nav-content-container'
-                    style={{
-                        display: 'flex',
-                        alignItems: 'center',
-                        paddingTop: 10
-                    }}>
+            <div className='user-profile-main-content' >
+                <div className='user-profile-nav-content-container'>
                     <UserProfileProvider>
                         <UserSideBar />
                         <UserProfileMainContent />
