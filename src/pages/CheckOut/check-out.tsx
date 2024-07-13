087bd4fd72b9a4d3e4266fdf2fc9d90fe5f47ae5
REICHAN\phuon <thaottpse173309@fpt.edu.vn>
2024-07-13 21:45:24 +0700

    checkout page

--- src/pages/CheckOut/check-out.tsx
+++ src/pages/CheckOut/check-out.tsx
@@ -1,14 +1,127 @@
-import React, { useEffect, useState } from 'react';
-import './check-out.css';
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+import "./check-out.css";
+import { TextField } from "@mui/material";
+type Inputs = {
+  name: string;
+  email: string;
+  phone: number;
+  terms: boolean;
+};
+const CheckOut = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    trigger,
+  } = useForm<Inputs>();
 
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-const CheckOut: React.FC = () => {
+  const inputStyle = {
+    margin: "8px 0",
+  };
 
-    return (
-        <>
-            <h1>hcekout</h1>
-        </>
-    );
+  // Fake order data
+  const orderItems = [
+    {
+      name: "Grooming Cat",
+      price: 95.0,
+      imageUrl:
+        "https://th.bing.com/th/id/OIP.e-FTPwDuLKTcXAS7iOAwQAAAAA?rs=1&pid=ImgDetMain",
+    },
+    {
+      name: "Grooming Cat",
+      price: 95.0,
+      imageUrl:
+        "https://th.bing.com/th/id/OIP.e-FTPwDuLKTcXAS7iOAwQAAAAA?rs=1&pid=ImgDetMain",
+    },
+  ];
+  const total = orderItems.reduce((total, item) => total + item.price, 0);
+
+  return (
+    <div className="checkout-container">
+      <h1>Checkout</h1>
+      <div className="checkout-content">
+        <form onSubmit={handleSubmit(onSubmit)} className="checkout-form">
+          <div className="contact-information">
+            <h2>Contact information</h2>
+            <TextField
+              sx={inputStyle}
+              required
+              id="name"
+              label="Name"
+              {...register("name", {
+                required: "Name is required",
+              })}
+              error={!!errors.name}
+              helperText={errors.name ? errors.name.message : ""}
+              onBlur={() => trigger("name")}
+              fullWidth
+            />
+            <TextField
+              sx={inputStyle}
+              required
+              id="email"
+              label="Email Address"
+              {...register("email", {
+                required: "Email is required",
+                pattern: {
+                  value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                  message: "Email is not valid",
+                },
+              })}
+              error={!!errors.email}
+              helperText={errors.email ? errors.email.message : ""}
+              onBlur={() => trigger("email")}
+              fullWidth
+            />
+            <TextField
+              sx={inputStyle}
+              required
+              id="phone"
+              label="Phone number"
+              {...register("phone", {
+                required: "Phone is required",
+                pattern: {
+                  value: /^[0-9]{10,11}$/,
+                  message: "Phone number must be 10-11 positive digits",
+                },
+              })}
+              error={!!errors.phone}
+              helperText={errors.phone ? errors.phone.message : ""}
+              onBlur={() => trigger("phone")}
+              fullWidth
+            />
+            <div className="payment-option vnpay">
+              <img
+                src="https://vinadesign.vn/uploads/images/2023/05/vnpay-logo-vinadesign-25-12-57-55.jpg"
+                alt="VNPAY"
+              />
+              <h5>You will pay by VNPAY</h5>
+            </div>
+            <button type="submit">Pay</button>
+          </div>
+          <div className="order-summary">
+            <h2>Order Summary</h2>
+            {orderItems.map((item, index) => (
+              <div className="order-item" key={index}>
+                <img src={item.imageUrl} alt={item.name} />
+                <div>
+                  <p>{item.name}</p>
+                  <p>${item.price.toFixed(2)}</p>
+                </div>
+              </div>
+            ))}
+
+            <div className="order-total">
+              <p>Total: ${total.toFixed(2)}</p>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default CheckOut;
--- src/pages/RegisterPage/register.tsx
+++ src/pages/RegisterPage/register.tsx
@@ -1,167 +1,181 @@
 import React from "react";
 import { useForm, SubmitHandler } from "react-hook-form";
 import "./register.css";
-import { Box, TextField, Button, Typography, Checkbox, FormControlLabel } from "@mui/material";
+import {
+  Box,
+  TextField,
+  Button,
+  Typography,
+  Checkbox,
+  FormControlLabel,
+} from "@mui/material";
+import GoogleIcon from "@mui/icons-material/Google";
 
 type Inputs = {
-    name: string;
-    email: string;
-    phone: number;
-    password: string;
-    passwordConfirm: string;
-    terms: boolean;
+  name: string;
+  email: string;
+  phone: number;
+  password: string;
+  passwordConfirm: string;
+  terms: boolean;
 };
 
 const Register: React.FC = () => {
-    const {
-        register,
-        handleSubmit,
-        watch,
-        formState: { errors },
-        trigger,
-    } = useForm<Inputs>();
-    
-    const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    trigger,
+  } = useForm<Inputs>();
 
-    const inputStyle = {
-        margin: "8px 0",
-    };
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
 
-    return (
-        <Box
-            component="form"
-            className="register-form"
-            onSubmit={handleSubmit(onSubmit)}
-            sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                alignItems: 'center',
-                maxWidth: '320px',
-                margin: 'auto',
-                padding: '20px',
-                backgroundColor: 'white',
-                borderRadius: '10px',
-                boxShadow: '0 4px 8px rgba(0,0,0,0.1)'
-            }}
-        >
-            <Typography
-                sx={{
-                    marginBottom: "10px",
-                    textAlign: "center",
-                }}
-                variant="h4"
-                gutterBottom
-            >
-                Register
-            </Typography>
+  const inputStyle = {
+    margin: "8px 0",
+  };
 
-            <TextField
-                sx={inputStyle}
-                required
-                id="name"
-                label="Username"
-                {...register("name", { 
-                    required: "Username is required",
-                    minLength: {
-                        value: 5,
-                        message: "Username must be at least 5 characters"
-                    },
-                    pattern: {
-                        value: /^[a-zA-Z][a-zA-Z0-9_!@#$%^&*()-+=]*$/,
-                        message: "Username must start with a letter"
-                    }
-                })}
-                error={!!errors.name}
-                helperText={errors.name ? errors.name.message : ""}
-                onBlur={() => trigger("name")}
+  return (
+    <Box
+      component="form"
+      className="register-form"
+      onSubmit={handleSubmit(onSubmit)}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        maxWidth: "320px",
+        margin: "auto",
+        padding: "20px",
+        backgroundColor: "white",
+        borderRadius: "10px",
+        boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+      }}
+    >
+      <Typography
+        sx={{
+          marginBottom: "10px",
+          textAlign: "center",
+          fontFamily: "initial",
+        }}
+        variant="h5"
+        gutterBottom
+      >
+        Join our pet-loving family
+      </Typography>
 
-                fullWidth
-            />
-            <TextField
-                sx={inputStyle}
-                required
-                id="email"
-                label="Email Address"
-                {...register("email", { 
-                    required: "Email is required",
-                    pattern: {
-                        value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-                        message: "Email is not valid"
-                    }
-                })}
-                error={!!errors.email}
-                helperText={errors.email ? errors.email.message : ""}
-                onBlur={() => trigger("email")}
-                fullWidth
-            />
-             <TextField
-                sx={inputStyle}
-                required
-                id="phone"
-                label="Phone number"
-                {...register("phone", { 
-                    required: "Phone is required",
-                    pattern: {
-                        value: /^[0-9]{10,11}$/,
-                        message: "Phone number must be 10-11 positive digits"
-                    }
-                })}
-                error={!!errors.phone}
-                helperText={errors.phone ? errors.phone.message : ""}
-                onBlur={() => trigger("phone")}
-                fullWidth
-            />
-            <TextField
-                sx={inputStyle}
-                required
-                id="password"
-                label="Password"
-                type="password"
-                {...register("password", { required: "Password is required" })}
-                error={!!errors.password}
-                helperText={errors.password ? errors.password.message : ""}
-                fullWidth
-            />
-            <TextField
-                sx={inputStyle}
-                required
-                id="passwordConfirm"
-                label="Confirm Password"
-                type="password"
-                {...register("passwordConfirm", { 
-                    required: "Please confirm your password",
-                    validate: (value) => {
-                        if (value === "") return "Password confirmation is required";
-                        if (value !== watch("password")) return "Passwords do not match";
-                        return true;
-                    }
-                })}
-                error={!!errors.passwordConfirm}
-                helperText={errors.passwordConfirm ? errors.passwordConfirm.message : ""}
-                fullWidth
-            />
+      <TextField
+        sx={inputStyle}
+        required
+        id="name"
+        label="Username"
+        {...register("name", {
+          required: "Username is required",
+          minLength: {
+            value: 5,
+            message: "Username must be at least 5 characters",
+          },
+          pattern: {
+            value: /^[a-zA-Z][a-zA-Z0-9_!@#$%^&*()-+=]*$/,
+            message: "Username must start with a letter",
+          },
+        })}
+        error={!!errors.name}
+        helperText={errors.name ? errors.name.message : ""}
+        onBlur={() => trigger("name")}
+        fullWidth
+      />
+      <TextField
+        sx={inputStyle}
+        required
+        id="email"
+        label="Email Address"
+        {...register("email", {
+          required: "Email is required",
+          pattern: {
+            value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+            message: "Email is not valid",
+          },
+        })}
+        error={!!errors.email}
+        helperText={errors.email ? errors.email.message : ""}
+        onBlur={() => trigger("email")}
+        fullWidth
+      />
+      <TextField
+        sx={inputStyle}
+        required
+        id="phone"
+        label="Phone number"
+        {...register("phone", {
+          required: "Phone is required",
+          pattern: {
+            value: /^[0-9]{10,11}$/,
+            message: "Phone number must be 10-11 positive digits",
+          },
+        })}
+        error={!!errors.phone}
+        helperText={errors.phone ? errors.phone.message : ""}
+        onBlur={() => trigger("phone")}
+        fullWidth
+      />
+      <TextField
+        sx={inputStyle}
+        required
+        id="password"
+        label="Password"
+        type="password"
+        {...register("password", { required: "Password is required" })}
+        error={!!errors.password}
+        helperText={errors.password ? errors.password.message : ""}
+        fullWidth
+      />
+      <TextField
+        sx={inputStyle}
+        required
+        id="passwordConfirm"
+        label="Confirm Password"
+        type="password"
+        {...register("passwordConfirm", {
+          required: "Please confirm your password",
+          validate: (value) => {
+            if (value === "") return "Password confirmation is required";
+            if (value !== watch("password")) return "Passwords do not match";
+            return true;
+          },
+        })}
+        error={!!errors.passwordConfirm}
+        helperText={
+          errors.passwordConfirm ? errors.passwordConfirm.message : ""
+        }
+        fullWidth
+      />
 
-            <FormControlLabel
-                control={
-                    <Checkbox 
-                        {...register("terms", { required: "You must agree to the terms and conditions" })} 
-                        color="primary" 
-                    />
-                }
-                label="Agree to our Terms and Conditions"
-                sx={{ alignSelf: 'flex-start' }}
-            />
-            {errors.terms && <Typography color="error">{errors.terms.message}</Typography>}
+      <FormControlLabel
+        control={
+          <Checkbox
+            {...register("terms", {
+              required: "You must agree to the terms and conditions",
+            })}
+            color="primary"
+          />
+        }
+        label="Agree to our Terms and Conditions"
+        sx={{ alignSelf: "flex-start" }}
+      />
+      {errors.terms && (
+        <Typography color="error">{errors.terms.message}</Typography>
+      )}
 
-            <Button type="submit" variant="contained" sx={{ marginTop: '10px' }}>
-                Register
-            </Button>
+      <Button type="submit" variant="contained" sx={{ marginTop: "10px" }}>
+        Register
+      </Button>
 
-            <Typography sx={{ marginTop: '20px' }}>
-                <a href="/login">Have an account? Login</a>
-            </Typography>
-        </Box>
-    );
-}
+      <Typography sx={{ marginTop: "20px" }}>
+        <a href="/login">Have an account? Login</a>
+      </Typography>
+    </Box>
+  );
+};
 
 export default Register;
